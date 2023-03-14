e66042ceab21e8e3d7a986135d2e3f1bd2edf8ad
nanFeng2023 <qqaazzww2023@163.com>
2023-03-14 18:30:24 +0800

    广告版本bug修复

--- app/src/main/java/com/ssv/signalsecurevpn/util/FirebaseUtils.kt
+++ app/src/main/java/com/ssv/signalsecurevpn/util/FirebaseUtils.kt
@@ -13,8 +13,12 @@ object FirebaseUtils {
                 val serviceVpnData = remoteConfig.getString("sigvn_ser")
                 SharePreferenceUtil.putString(AdMob.SIGVN_SERVICE, serviceVpnData)
 
+                val smartServiceVpnData = remoteConfig.getString("sigvn_smar")
+                SharePreferenceUtil.putString(AdMob.SIGVN_SMART_SERVICE, smartServiceVpnData)
+
                 val adData = remoteConfig.getString("sigvn_ad")
                 SharePreferenceUtil.putString(AdMob.SIGVN_AD, adData)
+
             }
         }
     }
